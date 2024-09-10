40aa1fd7b421fdc1973e031965d76435ffd9840c
Romulo Tole <tolemaicor@gmail.com>
2024-09-09 22:08:24 -0400

    Agregando quinto commit

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from src.routers import user
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-
 def run_flask_app():
     app.register_blueprint(user.main, url_prefix = '/user')
     app.run()
