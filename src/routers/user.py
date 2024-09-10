47d8f4b960f3839f606268c2c9e6d4d163185088
Romulo Tole <tolemaicor@gmail.com>
2024-09-09 23:42:53 -0400

    Agregando once commit

--- src/routers/user.py
+++ src/routers/user.py
@@ -5,3 +5,7 @@ main = Blueprint('user_blueprint', __name__)
 @main.route('/demo')
 def demo():
     return "<h1> Hola mundo jajaja </h1>"
+
+@main.route('/heroku')
+def heroku():
+    return "<h1> Heroku </h1>"
